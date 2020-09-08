2aa60a6e277895765ff96593b9879bac68ea84a9
dk4jo3 <dk4jo3@gmail.com>
2020-09-08 10:04:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,35 @@ import json
 # get top 100 coins and save the list of the names
 # get past 60 days of data (hourly) and get std of each. 
 
+list_cap = 50 
+date_cutoff = 60
+std_cutoff = 5
+coin_list = []
 
-current_time = int(time.time())
-print (current_time)
 
-# get past hour of data (minutely) and add them together. them append to dict 
+end_point = 'https://api.coingecko.com/api/v3/coins'
+volume_end_point = end_point + '{}/market_chart?vs_currency={}&days={}'.format(coin_id, against, days)
+coin_list_end_point = end_point + ('/markets?vs_currency=usd&order=market_cap_desc&per_page={}&page=1&sparkline=false').format(list_cap)
 
-# for loop to compare the last hourly data to std. 
+def get_coin_list():
+	api_response = requests.get(coin_list_end_point)
+	api_data = api_response.json()
+
+	for i in api_data:
+		coin_list.append(i['id'])
+
+	return coin_list
+
+
+get_coin_list()
+print (coin_list)
+
+# get past hour of data (minutely) and add them together
+
+# for loop to compare the last hourly data to the std. 
 # append to outlier list if it's over STD_CUT times of the std 
-# append the name, volume, price, ranking, and how many times of std is  over. 
+# append the name, volume, price, ranking, and how many times of std is over. 
+
+# save result in dict json them save 
 
 
-end_point = 'https://api.coingecko.com/api/v3/'
